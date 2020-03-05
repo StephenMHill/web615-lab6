f9897b22232bc1e4337e980b5baed1fe09f17e9d
Stephen Hill <xhillx99@gmail.com>
2020-03-05 12:56:45 -0500

    updated gem file and environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.middleware.use(Rack::Tracker) do
+    handler :google_analytics, { tracker: 'U-XXXXX-Y' }
+  end
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
